0425ad880480979ee2d06c09c0ee130398e9302d
Francis <mufraggie@gmail.com>
2022-08-19 11:10:39 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(newName){
+   customerName= customerName.toUpperCase();
+   return customerName;
+}
+function setBestCustomer(name){
+    bestCustomer="not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob";
+    return bestCustomer;
+}
+
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer= "Someone";
+    leastFavoriteCustomer= "Fred";
+    return leastFavoriteCustomer;
+
+}
